22b46dc7e02591660172ddea7b39485058da39f4
artem-bogdantsev <artem.bogdantsev@allianz.de>
2018-11-25 19:29:49 +0100

    Added intervals class as a module

---
+++ intervals.py
@@ -0,0 +1,10 @@
+class Intervals:
+    def __init__(self, intervals):
+        self.intervals = intervals
+        self.merged = []
+
+    def print_intervals(self):
+        print(self.intervals)
+
+    def merge(self):
+        return self.merged
