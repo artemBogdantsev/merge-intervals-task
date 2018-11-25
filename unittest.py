accfebba409654b22c5a4054b349e103df8be7c7
artem-bogdantsev <artem.bogdantsev@allianz.de>
2018-11-25 16:45:09 +0100

    TDD first :) Adding unit test

---
+++ unittest.py
@@ -0,0 +1,21 @@
+import unittest
+import merge
+
+class TestMerge(unittest.TestCase):
+    def __init__(self):
+        print()
+
+    def test_right_result(self):
+        '''
+        To test the merge module on passing with the right result
+        :return:
+        '''
+
+    def test_wrong_result(self):
+        '''
+        to test the merge module on not passing with the wrong result
+        :return:
+        '''
+
+if __name__ == '__main__':
+    unittest.main()
